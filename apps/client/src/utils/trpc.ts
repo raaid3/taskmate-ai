7682716e02203ec3648cc50a5424d437b7de8eb4
Dhyan Rai <172551908+raaid3@users.noreply.github.com>
2025-07-15 17:16:40 -0400

    add superjson for date parsing

--- apps/client/src/utils/trpc.ts
+++ apps/client/src/utils/trpc.ts
@@ -1,10 +1,22 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import { createTRPCClient, httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import type { AppRouter } from "../../../api/src/trpc/router.js";
+import superjson from "superjson";
 export const queryClient = new QueryClient();
 const trpcClient = createTRPCClient<AppRouter>({
-  links: [httpBatchLink({ url: "http://localhost:3000/trpc" })],
+  links: [
+    loggerLink({
+      enabled: (opts) =>
+        (process.env.NODE_ENV === "development" &&
+          typeof window !== "undefined") ||
+        (opts.direction === "down" && opts.result instanceof Error),
+    }),
+    httpBatchLink({
+      url: "http://localhost:3000/trpc",
+      transformer: superjson,
+    }),
+  ],
 });
 export const trpc = createTRPCOptionsProxy<AppRouter>({
   client: trpcClient,
