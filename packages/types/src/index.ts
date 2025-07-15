0b8b292ce9fd3ff95fa9fae588efbb12420fafc0
Dhyan Rai <172551908+raaid3@users.noreply.github.com>
2025-07-15 18:18:26 -0400

    Refactor todo types and schemas

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,10 +1,19 @@
 import { z } from "zod";
 
 export const TodoItemSchema = z.object({
-  dateTime: z.string(),
-  description: z.string(),
-  duration: z.number(),
-  recurring: z.boolean(),
+  id: z.number().int(),
+  title: z.string(),
+  description: z.string().nullable().optional(),
+  startDateTime: z.date({ error: "Not a valid date" }),
+  endDateTime: z.date({ error: "Not a valid date" }).nullable().optional(),
+  isRecurring: z.boolean(),
+  recurrenceRule: z.string().nullable().optional(),
+  authorId: z.number().int(),
 });
 
+export const TodoItemCreateSchema = TodoItemSchema.omit({
+  id: true,
+  authorId: true,
+});
+export type TodoItemCreate = z.infer<typeof TodoItemCreateSchema>;
 export type TodoItem = z.infer<typeof TodoItemSchema>;
