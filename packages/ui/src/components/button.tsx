bb0f8e963b946d5baf9ff8c6ded160234ac06ab4
Dhyan Rai <172551908+raaid3@users.noreply.github.com>
2025-08-07 01:56:50 -0400

    Minor fixes

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -1,17 +1,12 @@
-import type { ReactNode, MouseEventHandler } from "react";
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-interface ButtonProps {
-  children: ReactNode;
-  onClick: MouseEventHandler<HTMLButtonElement>;
-}
-
-export default function Button({ children, onClick }: ButtonProps) {
+export default function Button(props: ButtonProps) {
   return (
     <button
       className="px-6 py-2 rounded-full text-white font-bold bg-gradient-to-r from-purple-500 to-indigo-600 hover:shadow-[0_0_20px_#8A2BE2] transition-shadow duration-300"
-      onClick={onClick}
+      {...props}
     >
-      {children}
+      {props.children}
     </button>
   );
 }
