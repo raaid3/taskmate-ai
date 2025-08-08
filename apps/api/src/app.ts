b85d51a929ec37a76b6295dca26f23ff2e3aa034
Dhyan Rai <172551908+raaid3@users.noreply.github.com>
2025-08-08 12:33:32 -0400

    Update port and add start script

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -5,7 +5,7 @@ import { appRouter } from "./trpc/router.js";
 import { createContext } from "./trpc/context.js";
 import "dotenv/config";
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 console.log("Allowed origins: ", process.env.ALLOWED_ORIGINS);
 app.use(cors({ origin: process.env.ALLOWED_ORIGINS?.split(",") }));
 
@@ -22,5 +22,5 @@ app.use(
 );
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on por t: ${PORT}`);
 });
