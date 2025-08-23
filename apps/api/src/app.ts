ef0f7f6de4105fc8334a3b795d3c699931929a3a
Dhyan Rai <172551908+raaid3@users.noreply.github.com>
2025-08-22 23:39:37 -0400

    Change api default port

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -5,7 +5,7 @@ import { appRouter } from "./trpc/router.js";
 import { createContext } from "./trpc/context.js";
 import "dotenv/config";
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 7860;
 console.log("Allowed origins: ", process.env.ALLOWED_ORIGINS);
 app.use(cors({ origin: process.env.ALLOWED_ORIGINS?.split(",") }));
 
@@ -21,6 +21,6 @@ app.use(
   })
 );
 
-app.listen(PORT, () => {
-  console.log(`Server running on por t: ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port: ${PORT}`);
 });
